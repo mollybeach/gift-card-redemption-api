a867876fde28e1c6ae1f1c0ca20201b97aebe199
mollybeach <mollyjbeach@gmail.com>
2022-01-21 17:25:20 -0800

    initial commit

---
+++ api.js
@@ -0,0 +1,5 @@
+/*
+write an api that grabs the powerhouse-brewery.csv files using fs and reduces the data too the credit card number and the unused value "Unredeemed 
+Amount" and the  "Current Balance 
+As Of 01/13/2022" */
+
